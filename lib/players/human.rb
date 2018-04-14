c64ad78fb3a94a3b308c98b1bd9e0738ede47b58
Patrick J Louis <lumenfuzz@gmail.com>
2018-04-14 16:46:33 +0000

    automatically backed up by learn

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -3,6 +3,8 @@ class Players
 
     def move(board)
       board.display
+      puts ""
+      puts "You are player #{self.token}."
       puts "Which move would you like to make? (Enter a value from 1 to 9.): "
       return gets.chomp
     end
