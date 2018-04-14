24593333dd7a08b931440570a3cc2b24e60a0920
Patrick J Louis <lumenfuzz@gmail.com>
2018-04-14 15:31:06 +0000

    automatically backed up by learn

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -2,6 +2,7 @@ class Players
   class Human < Player
 
     def move(board)
+      board.display
       puts "Which move would you like to make? (Enter a value from 1 to 9.): "
       return gets.chomp
     end
