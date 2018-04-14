5a6dd1e50290791997c9cbd7495367ab6dbdb2dd
Patrick J Louis <lumenfuzz@gmail.com>
2018-04-14 16:19:13 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -4,20 +4,27 @@ class Players
   class Computer < Player
 
     WIN_COMBINATIONS = [
-      [0,1,2],
-      [3,4,5],
-      [6,7,8],
-      [0,3,6],
-      [1,4,7],
-      [2,5,8],
-      [0,4,8],
-      [6,4,2]
+      [3,5],
+      [1,7],
+      [0,8],
+      [6,2]
     ]
 
     def move(board)
       if board.cells[4] == " "
         return "5"
-      else
+      elsif board.cells[4] == self.token
+        WIN_COMBINATIONS.each do |combo|
+          potential_win = false
+          combo.each do |slot|
+            potential_win = true if board.cells[slot] == self.token
+          end
+          if potential_win
+            combo.each do |slot|
+              return (slot+1).to_s if board.cells[slot] == " "
+            end
+          end
+        end    
         move = 1
         board.cells.each do |cell|
           return move.to_s if cell == " "
