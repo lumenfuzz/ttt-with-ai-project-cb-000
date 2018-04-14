ad91e0b4cc64df37c320da4028de3248b1bd3231
Patrick J Louis <lumenfuzz@gmail.com>
2018-04-14 15:49:54 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,12 +1,24 @@
 class Players
   class Computer < Player
 
+    WIN_COMBINATIONS = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [6,4,2]
+    ]
+
     def move(board)
       if board.cells[5] == " "
         return "4"
       else
         move = 0
         board.cells.each do |cell|
+          binding.pry
           return move.to_s if cell == " "
           move += 1
         end
